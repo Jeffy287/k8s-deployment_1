7d37b6ca724fa37a4ff66d1b5fac9d95df3f664b
Jeffy287 <jefrinpeter287@gmail.com>
2025-06-21 19:41:27 +0530

    testing

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-#uvicorn main:app --reload
-### FastAPI application to render a form using Jinja2 templates
-from fastapi import FastAPI, Request, Form
+# main.py
 
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="/code")
 
-@app.get("/")
-
+@app.get("/", response_class=HTMLResponse)
 def form_post(request: Request):
-
-return templates.TemplateResponse('form.html', context={'request': request})
+    return templates.TemplateResponse("form.html", {"request": request})
